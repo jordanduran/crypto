e48a33f1c8780dccfd1f7d559657dabe6c357040
Jordan Duran <jordanduran@Jordans-MacBook-Pro.local>
2021-10-15 11:58:04 -0400

    App css file and import

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import { Switch, Route, Link } from 'react-router-dom';
 import { Layout, Typography, Space } from 'antd';
 import { Navbar } from './components';
+import './App.css';
 
 const App = () => {
   return (
-    <div>
+    <div className='app'>
       <div className='navbar'>
         <Navbar />
       </div>
