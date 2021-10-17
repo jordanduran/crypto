7a7581cd01ab634b21644253d31141b6cfe1b9ca
Jordan Duran <jordanduran@Jordans-MacBook-Pro.local>
2021-10-17 13:12:35 -0400

    Fixed typo in cryptoNewsApi envs

--- src/services/cryptoNewsApi.js
+++ src/services/cryptoNewsApi.js
@@ -2,11 +2,11 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 const cryptoNewsHeaders = {
   'x-bingapis-sdk': 'true',
-  'x-rapidapi-host': process.env.REACT_APP_CRYPTO_CRYPTO_NEWS_API_HOST,
-  'x-rapidapi-key': process.env.REACT_APP_CRYPTO_CRYPTO_NEWS_API_KEY,
+  'x-rapidapi-host': process.env.REACT_APP_CRYPTO_NEWS_API_HOST,
+  'x-rapidapi-key': process.env.REACT_APP_CRYPTO_NEWS_API_KEY,
 };
 
-const baseUrl = process.env.REACT_APP_CRYPTO_CRYPTO_NEWS_API_BASE_URL;
+const baseUrl = process.env.REACT_APP_CRYPTO_NEWS_API_BASE_URL;
 
 const createRequest = (url) => ({ url, headers: cryptoNewsHeaders });
 
